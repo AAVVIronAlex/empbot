2fb6b66fd8b7a0c36abbe87cb860ebcb0d0accad
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-06-30 12:32:34 +0400

    treewide: Fix envvars, change dependencies, add gitignore
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import os
 import telebot
+from dotenv import load_dotenv
 
-API_KEY = os.getenv('API_KEY')
-bot = telebot.TeleBot(API_KEY)
+load_dotenv("config.env")
 
-@bot.message_handler(commands=['greet'])
+bot_token = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(bot_token)
+
+
+@bot.message_handler(commands=["greet"])
 def greet(message):
-  bot.reply_to(message, "Hey! What up?")
+    bot.reply_to(message, "Hey! What up?")
 
-@bot.message_handler(commands=['emp'])
+
+@bot.message_handler(commands=["emp"])
 def emp(message):
-  bot.send_message(message.chat.id, "earthmapspictures.weebly.com")
+    bot.send_message(message.chat.id, "earthmapspictures.weebly.com")
 
-@bot.message_handler(commands=['hello'])
+
+@bot.message_handler(commands=["hello"])
 def hello(message):
-  bot.send_message(message.chat.id, "Hello!")
+    bot.send_message(message.chat.id, "Hello!")
 
-@bot.message_handler(commands=['waka'])
+
+@bot.message_handler(commands=["waka"])
 def waka(message):
-  bot.send_message(message.chat.id, "Waka Woka Wiki Weke Woko Weki Wake ...")
+    bot.send_message(message.chat.id, "Waka Woka Wiki Weke Woko Weki Wake ...")
 
-@bot.message_handler(commands=['about'])
+
+@bot.message_handler(commands=["about"])
 def hello(message):
-  bot.send_message(message.chat.id, "EarthMapsPictures Corporation!")
+    bot.send_message(message.chat.id, "EarthMapsPictures Corporation!")
+
 
 bot.polling()
