7050994992170569e879926ee3397242beabffe1
AAVVIronAlex <82303102+AAVVIronAlex@users.noreply.github.com>
2021-07-04 19:15:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import telebot
 import random
+import string
 from dotenv import load_dotenv
 
 load_dotenv("config.env")
@@ -9,6 +10,24 @@ bot_token = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(bot_token)
 
 jokes = ["I was her semicolon but...    One day She decided to switch to Python.", "I'm on a seafood diet....   I see food and I wanna eat it.", "*Dad thinks you are gaming on the pc* Dad: Are you winning, son?  There is no winning with these syntax errors dad!"]
+
+adjectives = ['sleepy', 'slow', 'smelly', 'wet', 'fat', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'fluffy', 'white', 'proud', 'brave']
+nouns = ['apples', 'dinosaur', 'ball', 'toaster', 'goat', 'dragon', 'hammer', 'duck', 'panda', 'telephone', 'banana', 'teacher']
+colours = ['blue', 'red', 'green', 'yellow', 'purple', 'magenta', 'black', 'white', 'orange', 'cyan', 'brown', 'silver', 'gold']
+
+while True:
+    for num in range(3):
+        adjective = random.choice(adjectives)
+        noun = random.choice(nouns)
+        number = random.randrange(0, 100)
+        colour = random.choice(colours)
+        special_char = random.choice(string.punctuation)
+
+password = adjective + colour + noun + str(number) + special_char
+
+@bot.message_handler(commands=["newpassword"])
+def greet(message):
+    bot.reply_to(message, "Your password is: %s" password)
 
 @bot.message_handler(commands=["greet"])
 def greet(message):
