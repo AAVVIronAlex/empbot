c9fdde8bac95376465f5528ceb874f35d9863e11
Alex <82303102+AAVVIronAlex@users.noreply.github.com>
2021-09-14 09:55:59 +0400

    Update misc.py

--- emp/modules/misc.py
+++ emp/modules/misc.py
@@ -1,6 +1,9 @@
 from emp import bot
 import random
 import string
+from pyrogram import  Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
 from html import escape
 
 # CONSTANTS
@@ -92,3 +95,47 @@ def joke(message):
 @bot.message_handler(commands=["alex"])
 def alex(message):
     bot.send_message(message.chat.id, "Alex has lost over 500GBs of data and is trying to recover it he is busy don't bother him for free recovery software please DM him on any platform. Thanks.")
+
+    @bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    TEXT = "Hai {} \n\n**I Am Password Generator Bot. I Can Generate Strong Passwords At Your Wish Length (Max. 84).** \n\nFor Know More /help"
+    BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("Channel 🔰", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Support Group ⭕️", url = "https://telegram.me/ekbotz_support")],[InlineKeyboardButton("Repo 🗂️", url = "https://github.com/M-fazin/Password-Generator-Bot"),InlineKeyboardButton("Deploy 🗃️", url = "https://heroku.com/deploy?template=https://github.com/M-fazin/Password-Generator-Bot")],[InlineKeyboardButton("Developer 💡", url = "https://github.com/M-fazin/")]])
+    await update.reply_text(
+        text=TEXT.format(update.from_user.mention),
+        reply_markup=BUTTONS,
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+    #Password Generator Rev.2
+    
+@bot.on_message(filters.private & filters.command(["help"]))
+async def help(bot, update):
+    HELP = "Hello, I am EMP Bot password creator Rev. 2.0. \nMaximum Send Digit limit a random password will be generated in that limit. \nAllowed Digits Till 84"
+   
+    await update.reply_text(
+        text=HELP.format(update.from_user.mention),
+        reply_markup=HELP_BUTTON,
+        disable_web_page_preview=True,
+        quote=True
+        )
+	
+@bot.on_message(filters.private & filters.text)
+async def password(bot, message):
+    password = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
+    try:
+       limit = int(message.text)
+    except:
+       pass    
+    if limit >= 85 or limit <= 0:
+        text = "Sorry... Failed To Create Password Because Limit is 1 to 84"
+    else:
+        randomValue = random.sample(password, limit)
+        randomValue = "".join(randomValue)
+        text = f"**Your Password Generated Succesfully.** \nYour Password Limit : `{limit}`. \nPassword 👇 :- \n`{randomValue}` "
+      
+    await message.reply_text(text, True)
+	
+
+			
+bot.run()
