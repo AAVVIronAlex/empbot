6599380e93256f667df404edd41bd7077b52b6dd
Alex <82303102+AAVVIronAlex@users.noreply.github.com>
2022-02-12 17:54:44 +0400

    Update about.py

--- emp/modules/about.py
+++ emp/modules/about.py
@@ -29,6 +29,6 @@ def emp(message):
 @bot.message_handler(commands=["about"])
 def about(message):
     bot.send_message(
-        message.chat.id, "Copyright EarthMapsPictures Corporation 2015-2021"
+        message.chat.id, "Copyright EarthMapsPictures Inc. 2015-2022"
     )
-    bot.send_message(message.chat.id, "Developed by @alexthegreatish and @zakaryan2004")
+    bot.send_message(message.chat.id, "Developed by @alexthegreatish and EarthMapsPictures Inc.")
