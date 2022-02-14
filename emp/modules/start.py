b09c24a61622f9178df13cdd5b6d01c94a43199f
Alex <82303102+AAVVIronAlex@users.noreply.github.com>
2022-02-14 20:56:01 +0400

    Update start.py

--- emp/modules/start.py
+++ emp/modules/start.py
@@ -1,6 +1,5 @@
 from emp import bot
 
-
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(
@@ -10,11 +9,9 @@ def start(message):
     )
     bot.send_message(message.chat.id, "Type /help to see what I can do!")
 
-
 @bot.message_handler(commands=["greet"])
 def greet(message):
     bot.reply_to(message, "Hey! What up?")
-
 
 @bot.message_handler(commands=["hello"])
 def hello(message):
