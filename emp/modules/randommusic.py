8f334e54f93b9439804c086d7a03774742127ceb
Alex <82303102+AAVVIronAlex@users.noreply.github.com>
2022-02-18 17:50:14 +0400

    Create randommusic.py
    Added random music. Some tracks.
    Alex V

---
+++ emp/modules/randommusic.py
@@ -0,0 +1,18 @@
+from emp import bot
+import random
+import string
+
+music = [
+    "https://www.youtube.com/watch?v=w0m0hTrtlWM&t=28s",
+    "https://www.youtube.com/watch?v=QK-Z1K67uaA",
+    "https://www.youtube.com/watch?v=SUzsih7QJSY",
+    "https://www.youtube.com/watch?v=qqK1FrO3BdM",
+    "https://www.youtube.com/watch?v=s6WGNd8QR-U",
+    "https://www.youtube.com/watch?v=-50cU8DqJzs",
+    "https://www.youtube.com/watch?v=F82nyLAAUrk", 
+]
+
+@bot.message_handler(commands=["randommusic"])
+def randommusic(message):
+    target = random.choice(music)
+    bot.send_message(message.chat.id, music)
