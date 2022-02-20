0385642a5c42e55e48c6110952afa369256bad92
Alex V <alex.babajanyan2015@gmail.com>
2022-02-20 13:02:40 +0400

    Version 2.0

--- emp/modules/girlsrating.py
+++ emp/modules/girlsrating.py
@@ -66,7 +66,31 @@ two_star = ["Խոջաբագյան Ելենա", "Շերոյան Մագդալեն�
 one_star = ["Այվազյան Ժաննա", "Թադևոսյան Ծովինար", ]
 zero_star = ["Ղահրամանյան Ինգա", ]
 
-@bot.message_handler(commands=["givemeagirl"])
+@bot.message_handler(commands = ["ratedgirl5"])
+def rated_girl(message):
+  target = random.choice(message.chat.id, five_star)
+
+@bot.message_handler(commands = ["ratedgirl4"])
+def rated_girl(message):
+  target = random.choice(message.chat.id, four_star)
+
+@bot.message_handler(commands = ["ratedgirl3"])
+def rated_girl(message):
+  target = random.choice(message.chat.id, three_star)
+
+@bot.message_handler(commands = ["ratedgirl2"])
+def rated_girl(message):
+  target = random.choice(message.chat.id, two_star)
+
+@bot.message_handler(commands = ["ratedgirl1"])
+def rated_girl(message):
+  target = random.choice(message.chat.id, one_star)
+
+@bot.message_handler(commands = ["ratedgirl0"])
+def rated_girl(message):
+  target = random.choice(message.chat.id, zero_star)
+
+@bot.message_handler(commands = ["givemeagirl"])
 def givemeagirl(message):
   target = random.choice(girls)
   bot.send_message(message.chat.id, target)
