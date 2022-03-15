30f761e18d7aa5f1e5a1265dc0020a873f9a2451
Alex V <alex.babajanyan2015@gmail.com>
2022-03-15 19:02:28 +0400

    Added more contact info. Alex V

--- emp/modules/misc.py
+++ emp/modules/misc.py
@@ -156,7 +156,7 @@ def newpassword_ultra_2(message):
 
 @bot.message_handler(commands = ["contact"])
 def contact(message):
-    bot.send_message(message.chat.id, "Telegram - @alexthegreatish \nFacebook - https://www.facebook.com/profile.php?id=100009196180783 \nGitHub - https://github.com/AAVVIronAlex \nInstagram - https://www.instagram.com/earthmapspictures/ \nReddit - https://www.reddit.com/user/AAVVIronAlex \nSpaceHey - https://spacehey.com/earthmapspictures \nTwitter - https://twitter.com/thealexgreater \nSpotify - https://open.spotify.com/user/1ygoezonte4i1hs73yj0qv0nb?si=dd1f41e818ba4cd5 \nYouTube - https://www.youtube.com/channel/UCcsUDEJ6HCUDSgT1gmEtxNQ/about \nMail - alex.babajanyan2015@gmail.com")
+    bot.send_message(message.chat.id, "Telegram - @alexthegreatish \nFacebook - https://www.facebook.com/profile.php?id=100009196180783 \nGitHub - https://github.com/AAVVIronAlex \nInstagram - https://www.instagram.com/earthmapspictures/ \nReddit - https://www.reddit.com/user/AAVVIronAlex \nSpaceHey - https://spacehey.com/earthmapspictures \nTwitter - https://twitter.com/thealexgreater \nSpotify - https://open.spotify.com/user/1ygoezonte4i1hs73yj0qv0nb?si=dd1f41e818ba4cd5 \nYouTube - https://www.youtube.com/channel/UCcsUDEJ6HCUDSgT1gmEtxNQ/about \nMail - alex.babajanyan2015@gmail.com \nLinusTechTips - https://linustechtips.com/profile/687179-aavvironalex/")
 
 @bot.message_handler(commands = "mathexam")
 def mathexam(message):
