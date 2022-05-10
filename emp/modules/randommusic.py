d7876e24adfc9a55af0f4a6e435c4fe77b472004
AAVVIronAlex <alex.babajanyan2015@gmail.com>
2022-05-10 12:00:00 +0400

    Added new music.

--- emp/modules/randommusic.py
+++ emp/modules/randommusic.py
@@ -67,7 +67,8 @@ music = [
     "https://www.youtube.com/watch?v=XbGs_qK2PQA",
     "https://www.youtube.com/watch?v=fJbALcAlijg",
     "https://www.youtube.com/watch?v=fHjMpT-K8u0",
-    "https://www.youtube.com/watch?v=dhYOPzcsbGM"
+    "https://www.youtube.com/watch?v=dhYOPzcsbGM",
+    "https://www.youtube.com/watch?v=T1q9Ub7y52g"
 ]
 
 @bot.message_handler(commands = ["randommusic"])
