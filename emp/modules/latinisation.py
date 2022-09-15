05dd9015ba526f26caa56074bd97697ff0e12576
AAVVIronAlex <alex.babajanyan2015@gmail.com>
2022-09-15 23:06:08 +0400

    Fixed some things, testing.

--- emp/modules/latinisation.py
+++ emp/modules/latinisation.py
@@ -1,17 +1,7 @@
 from emp import bot
 import random
 import string
-import asyncio
 
-new = 'random text'
-
-# start session
-
-async def main():
-    global new
-    messages = await client.get_messages('channel sample')
-    new = messages[0].text
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-print(new)  # message text
+def reply(update, content):
+    user_input = update.message.text
+    update.message.reply_text(f"The text you have entered {user_input}")
