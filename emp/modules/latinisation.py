beaa8e84c35f072d380f45c37ef1a67e3fcd34f3
AAVVIronAlex <alex.babajanyan2015@gmail.com>
2022-09-16 10:22:06 +0400

    Deleted the progress.

--- emp/modules/latinisation.py
+++ emp/modules/latinisation.py
@@ -2,10 +2,4 @@ from emp import bot
 import random
 import string
 
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-  cid = message.chat.id
-  mid = message.message_id 
-  message_text = message.text 
-  user_id = message.from_user.id 
-  user_name = message.from_user.first_name 
+#### nothing here
