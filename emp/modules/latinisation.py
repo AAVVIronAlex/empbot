f11bd71a9ae869f009e1a8134f53a4ad47b0c1ec
Alex V <alex.babajanyan2015@gmail.com>
2023-06-30 13:19:19 +0400

    Added the new latinisation command.

--- emp/modules/latinisation.py
+++ emp/modules/latinisation.py
@@ -2,4 +2,6 @@ from emp import bot
 import random
 import string
 
-#### nothing here
+@bot.message_handler(commands = ["latinisation"])
+def latinisation(message):
+    bot.send_message(message.chat.id, "This function is not yet available on the @earthmapspicturesbot, go check out @wakamastersbot to use it.")
